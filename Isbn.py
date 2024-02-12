87ba2646a6df5ce3436ad87c9ead3d7e2c81305e
dzuokumor <144033027+dzuokumor@users.noreply.github.com>
2024-02-12 15:48:07 +0100

    Update Isbn.py

--- Isbn.py
+++ Isbn.py
@@ -1,13 +1,11 @@
 import re
 
 def match_isbn_number():
-    # ISBN numbers: "ISBN xxx-x-xxx-xxxxx-x"
     isbn_pattern = r"ISBN \d{3}-\d-\d{3}-\d{5}-\d"
 
-    # Get user input
     input_string = input("Enter an ISBN number: ")
 
-    # Match against the regular expression pattern
+    # This matches against the regular expression pattern
     isbn_match = re.match(isbn_pattern, input_string)
 
     if isbn_match:
@@ -16,5 +14,5 @@ def match_isbn_number():
     else:
         print("Invalid ISBN Number")
 
-# Call the function
+# Lastly, we call the function
 match_isbn_number()
