b267b5d8d47c446ac654301a1caecafe715278fe
dav1d001 <144053473+dav1d001@users.noreply.github.com>
2024-02-12 14:57:52 +0100

    movies.py

---
+++ movies.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import re
+def movie_titles_match(titles):
+    titles = ""
+    movie_pattern = '(.*)\s((\d{4}))'
+    
+    is_movie_title = re.match(titles, movie_pattern)
+    if is_movie_title:
+        print(f"{titles} this is a movie")
+    else:
+        print(f"{titles} this is not a movie, please enter a proper movie name")
+
+
+# test 
+if __name__ == "__main__":
+    titles = ""
+    movie_titles_match(titles)
